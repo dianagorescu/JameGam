5e41b47f215e1c2ddf359c8f054217e7002056de
Aleks433 <alexgabriel879@gmail.com>
2024-04-07 01:03:53 +0300

    AI that follows

--- JameGam/Meow Knight/Assets/Scripts/Enemy.cs
+++ JameGam/Meow Knight/Assets/Scripts/Enemy.cs
@@ -1,11 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class Enemy : MonoBehaviour
 {
     public int health = 100;
-    // Start is called before the first frame update
+    public AIPath aiPath;
+
+    void Update()
+    {
+        if(aiPath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        } else if(aiPath.desiredVelocity.x <= -0.01f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+    }
+
+
     public void TakeDamage(int damage)
     {
         health -= damage;
